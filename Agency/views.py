816777fb5617286e05b1a2277b67232e79d4c9e7
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-03-17 18:08:39 +0300

    create about.html file

--- Agency/views.py
+++ Agency/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def frontpage(request):
     return render(request, 'Agency/frontpage.html')
+
+def about(request):
+    return render(request, 'Agency/about.html')
