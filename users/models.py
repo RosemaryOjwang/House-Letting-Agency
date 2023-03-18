9d7da317367638583fb2853cb164390080a23def
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-03-18 10:59:52 +0300

    create userprofile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User_Profile(models.Model):
+    user = models.OneToOneField(User,
+                                related_name='userprofile',
+                                on_delete=models.CASCADE)
