0b344cc0b4cd74bd6b0ce2ef80f03add3ff52768
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-03-18 12:00:13 +0300

    register House_Location model on the admin site

--- Houses/admin.py
+++ Houses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import House_Location
 
 # Register your models here.
+@admin.register(House_Location)
+class House_LocationAdmin(admin.ModelAdmin):
+    list_display = ['location', 'slug']
+    prepopulated_fields = {'slug': ('location',)}
--- Houses/models.py
+++ Houses/models.py
@@ -4,4 +4,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 class House_Location(models.Model):
     location = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255,
+                            unique=True)
+    
+    class Meta:
+        ordering = ['location']
+        indexes = [
+            models.Index(fields=['location']),
+        ]
+        verbose_name = 'House Location'
+        verbose_name_plural = 'House Locations'
+
+    def __str__(self):
+        return self.location
