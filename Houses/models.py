0f8d3e995849b2fb03836cfda6931e3e6d7fc970
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-03-18 11:38:55 +0300

    create House_Location model

--- Houses/models.py
+++ Houses/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class House_Location(models.Model):
+    location = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
