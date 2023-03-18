15743de2f79f6bd6c9b1c0e74a390f89ec938823
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-03-18 12:41:34 +0300

    create House_Details model

--- Houses/models.py
+++ Houses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phone_field import PhoneField
+
 
 # Create your models here.
 class House_Location(models.Model):
@@ -17,3 +19,26 @@ class House_Location(models.Model):
 
     def __str__(self):
         return self.location
+    
+class House_Details(models.Model):
+    user = models.ForeignKey(User,
+                             related_name='locations',
+                             on_delete=models.CASCADE)
+    location = models.ForeignKey(House_Location,
+                                 related_name='locations',
+                                 on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    thumbnail = models.ImageField(upload_to='img')
+    description = models.TextField(max_length=255)
+    monthly_rent = models.DecimalField(max_digits=10,
+                                       decimal_places=2)
+    available = models.BooleanField(default=True)
+    owners_contact = PhoneField(help_text='Enter contact phone number')
+    posted = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title']
+        
+    def __str__(self):
+        return self.title
