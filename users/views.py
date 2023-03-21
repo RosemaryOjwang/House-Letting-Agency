bdf87cbf73ce306c3e1c53a86066c5684fd2d396
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-03-21 11:38:42 +0300

    create owner_detail view in users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+def owner_detail(request, pk):
+    user = User.objects.get(pk=pk)
+
+    return render(request, 'userprofile/agent_detail.html',
+                  {'user': user})
