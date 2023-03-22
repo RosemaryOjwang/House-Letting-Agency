b58ce41281be6bf4ba123e5e420999116e15870f
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-03-22 14:32:44 +0300

    create user signup capability-work in progress

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import SignUpView
 
 urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', SignUpView.as_view(), name='login'),
     path('owners/<int:pk>/', views.owner_detail, name='owner_detail'),
-    path('register_owner/', views.register_owner, name='register_owner'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 from django.contrib.auth.models import User
 
 # Create your views here.
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "registration/signup.html"
 def owner_detail(request, pk):
     user = User.objects.get(pk=pk)
 
     return render(request, 'userprofile/owner_detail.html',
                   {'user': user})
-
-def register_owner(request):
-    return render(request, 'userprofile/register_owner.html')
