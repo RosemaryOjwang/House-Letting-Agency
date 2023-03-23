2d247353d62a7d4b64590e45184adb6d1da42ac4
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-03-24 01:46:47 +0300

    style navbar and House_DetailsForm, and add Logout, Add House and Edit house buttons in user_admin site

--- Houses/forms.py
+++ Houses/forms.py
@@ -5,3 +5,7 @@ class House_DetailsForm(forms.ModelForm):
     class Meta:
         model =House_Details
         fields = ('location', 'title', 'description', 'monthly_rent', 'thumbnail', 'owners_contact', 'available',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs.update({'class': 'description'})
