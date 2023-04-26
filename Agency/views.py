e4205482a875dccbcf2860c9bd7e3c867ce775fb
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-04-26 14:13:52 +0300

    add pagination to frontpage

--- Agency/views.py
+++ Agency/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from Houses.models import House_Location, House_Details
+from Houses.models import House_Details
+from django.core.paginator import Paginator
 
 def frontpage(request):
-    houses = House_Details.objects.exclude(available=False)
+    houses_list = House_Details.objects.exclude(available=False)
+    paginator = Paginator(houses_list, 10)
+    page_number = request.GET.get('page', 1)
+    listing = paginator.get_page(page_number)
 
     return render(request, 'Agency/frontpage.html',
-                  {'houses': houses})
+                  {'listing': listing})
 
 def about(request):
     return render(request, 'Agency/about.html')
