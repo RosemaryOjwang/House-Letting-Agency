f4de6117eb3e617093dd97ba614eff59ddb27e19
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-05-13 12:38:19 +0300

    edit allowed hosts

--- LettingAgency/settings.py
+++ LettingAgency/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
